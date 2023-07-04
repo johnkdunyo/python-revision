2bc1c0aefebfa46cb32e50894158111d45a9b309
johnkdunyo <johnkdunyo@gmail.com>
2023-07-04 12:28:43 +0000

    feature; reading and instantiating instances from a csv file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # store management project
+import csv
+
+
 class Item:
     pay_rate: 0.8
 
@@ -13,7 +16,7 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-        # actions to execute; this add any instance created to the all class attribute
+        # actions to execute; this adds any instance created to the all class attribute
         Item.all.append(self)
 
     def __repr__(self):
@@ -22,13 +25,20 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    # decorate to make the method a class method
+    # this reads from the csv file and instantiates new instances for each record
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            items = list(reader)
+
+        for item in items:
+            Item(item.get("name"), float(item.get("price")), int(item.get('quantity')))
+
 
 # test
-item1 = Item("laptop", 2.50, 4)
-print(item1.calculate_total_price())
 
-print(Item.__dict__)  # attributes at class level
-print(item1.__dict__)  # attributes at instance level
-
+Item.instantiate_from_csv()
 # prints all instance in the repr format in the class
 print(Item.all)
